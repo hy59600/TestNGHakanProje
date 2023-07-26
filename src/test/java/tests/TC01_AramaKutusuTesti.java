7d03364aa642287e17afba4e86374963455f25f6
hy59600 <hyurdagul59@gmail.com>
2023-07-26 18:09:06 +0200

    26Temmuz2023_V1

---
+++ src/test/java/tests/TC01_AramaKutusuTesti.java
@@ -0,0 +1,17 @@
+package tests;
+
+import org.openqa.selenium.Keys;
+import org.testng.annotations.Test;
+import pages.Page;
+import utilities.ConfigReader;
+import utilities.Driver;
+
+public class TC01_AramaKutusuTesti {
+    @Test
+    public void test01() {
+        Driver.getDriver().get(ConfigReader.getProperty("amazonUrl"));
+        Page page=new Page();
+        //Page.aramaKutusu.sendKeys("iphone", Keys.ENTER);
+
+    }
+}
